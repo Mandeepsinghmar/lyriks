b6da35b8e3b9b22bc62faf1b1f7f396d5036364e
Mandeep Singhmar <msingh8328@gmail.com>
2025-03-26 20:26:23 +0530

    fix: api issues

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,42 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const DetailsHeader = ({ artistId, artistData, songData }) => (
-  <div className="relative w-full flex flex-col">
-    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+const DetailsHeader = ({ artistId, artistData }) => {
+  console.log(artistData);
+  return (
+    <div className='relative w-full flex flex-col'>
+      <div className='w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28' />
 
-    <div className="absolute inset-0 flex items-center">
-      <img
-        alt="profile"
-        src={
-          artistId ? artistData?.artists[artistId].attributes?.artwork?.url
-            .replace('{w}', '500')
-            .replace('{h}', '500')
-            : songData?.images?.coverart
-}
-        className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
-      />
+      <div className='absolute inset-0 flex items-center'>
+        <img
+          alt='profile'
+          src={artistData?.avatar}
+          className='sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black'
+        />
 
-      <div className="ml-5">
-        <p className="font-bold sm:text-3xl text-xl text-white">
-          {artistId ? artistData?.artists[artistId].attributes?.name : songData?.title}
-        </p>
-        {!artistId && (
-          <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
-            <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
-          </Link>
-        )}
+        <div className='ml-5'>
+          <p className='font-bold sm:text-3xl text-xl text-white'>
+            {artistData?.attributes?.name}
+          </p>
+          <p className=' mt-2 text-sm text-gray-300 leading-relaxed'>
+            {artistData?.attributes?.artistBio}
+          </p>
+          {/* {!artistId && (
+            <Link to={`/artists/${songData?.relationships.artists.data[0].id}`}>
+              <p className='text-base text-gray-400 mt-2'>
+                {songData?.attributes.artistName}
+              </p>
+            </Link>
+          )} */}
 
-        <p className="text-base text-gray-400 mt-2">
-          {artistId
-            ? artistData?.artists[artistId].attributes?.genreNames[0]
-            : songData?.genres?.primary}
-        </p>
+          {/* <p className='text-base text-gray-400 mt-2'>
+            {artistId
+              ? artistData?.artists[artistId].attributes?.genreNames[0]
+              : songData?.genres?.primary}
+          </p> */}
+        </div>
       </div>
-    </div>
 
-    <div className="w-full sm:h-44 h-24" />
-  </div>
-);
+      <div className='w-full sm:h-44 h-24' />
+    </div>
+  );
+};
 
 export default DetailsHeader;
